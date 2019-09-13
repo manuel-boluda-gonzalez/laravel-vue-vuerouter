10a409b70f3e89af5ac22c54eeccc21746fffa8a
unknown <boludagonzalezmanuel@gmail.com>
2019-09-13 11:15:41 +0100

    ready to start

---
+++ resources/js/views/App.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        <router-link to="/">Home</router-link>
+        <router-link to="/about">About</router-link>
+
+
+        <router-view></router-view>
+    </div>
+</template>
+
+
+<script>
+export default {
+    
+}
+</script>
